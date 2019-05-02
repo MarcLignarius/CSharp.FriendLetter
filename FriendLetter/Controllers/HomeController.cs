d55011f064acb0b63fe2f7553d7ac207534bee3c
Marc Davies <marcdaviesriot@gmail.com>
2019-05-02 02:15:29 -0700

    add images

--- FriendLetter/Controllers/HomeController.cs
+++ FriendLetter/Controllers/HomeController.cs
@@ -6,26 +6,45 @@ namespace FriendLetter.Controllers
   public class HomeController : Controller
   {
 
-    [Route("/hello")]
-    public string Hello() { return "Hello friend!"; }
+      [Route("/hello")]
+      public string Hello()
+      {
+          return "Hello friend!";
+      }
 
-    [Route("/goodbye")]
-    public string Goodbye() { return "Goodbye friend."; }
+      [Route("/goodbye")]
+      public string Goodbye()
+      {
+          return "Goodbye friend.";
+      }
 
-    [Route("/postcard")]
-    public ActionResult Postcard(string recipient, string sender)
-    {
-      LetterVariable myLetterVariable = new LetterVariable();
-      myLetterVariable.SetRecipient(recipient);
-      myLetterVariable.SetSender(sender);
-      return View(myLetterVariable);
-    }
+      [Route("/postcard")]
+      public ActionResult Postcard(string recipient, string sender)
+      {
+          LetterVariable myLetterVariable = new LetterVariable();
+          myLetterVariable.SetRecipient(recipient);
+          myLetterVariable.SetSender(sender);
+          return View(myLetterVariable);
+      }
 
-    [Route("/journal")]
-    public ActionResult Journal() { return View(); }
+      [Route("/journal")]
+      public ActionResult Journal()
+      {
+          return View();
+      }
 
-    [Route("/form")]
-    public ActionResult Form() { return View(); }
+      [Route("/form")]
+      public ActionResult Form()
+      {
+          return View();
+      }
+
+      [Produces("text/html")]
+      [Route("/favorite_photos")]
+      public ActionResult FavoritePhotos()
+      {
+          return View();
+      }
 
   }
 }
